ce5b0029f0e84b14b8d512e83f678dce7ae2bf33
Shubham kumar <97464092+ShubhamWP07@users.noreply.github.com>
2023-06-05 21:35:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+function FirstNonRepeatingChar(s) {
+  let charCount = {};
+
+  for (let i = 0; i < s.length; i++) {
+    const ch = s[i];
+    charCount[ch] = (charCount[ch] || 0) + 1;
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    const ch = s[i];
+    if (charCount[ch] === 1) {
+      return i;
+    }
+  }
+
+  return -1; 
+}
